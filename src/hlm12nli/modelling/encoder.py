e8c02a2e022af70103362512dd72643ba8852815
Hudson Mendes <hudsonmendes@gmail.com>
2023-09-13 22:41:05 +0100

    checkpoint: initial first pass

---
+++ src/hlm12nli/modelling/encoder.py
@@ -0,0 +1,38 @@
+from typing import List
+import torch
+import transformers
+
+from .config import Hlm12NliConfig
+
+
+class Hlm12NliEncoder(transformers.PreTrainedModel):
+    def __init__(self, config: Hlm12NliConfig):
+        self.embeddings = torch.nn.Embedding(
+            num_embeddings=config.vocab_size,
+            embedding_dim=config.hidden_size,
+            padding_idx=config.pad_token_id,
+        )
+        self.lstm = torch.nn.LSTM(
+            input_size=config.hidden_size,
+            hidden_size=config.lstm_dims,
+            bidirectional=config.lstm_bidir,
+            batch_first=True,
+        )
+        self.mha = torch.nn.MultiheadAttention(
+            embed_dim=config.seq_len,
+            num_heads=config.attn_heads,
+            dropout=config.attn_dropout,
+            batch_first=True,
+        )
+        self.layer_norm = torch.nn.LayerNorm(config.hidden_size)
+
+    def forward(self, x: List[str]) -> torch.Tensor:
+        y = self.embeddings(input_ids)
+        y, _ = self.lstm(y)
+        mha_out, _ = self.mha(
+            lstm_out,
+            lstm_out,
+            lstm_out,
+            key_padding_mask=attention_mask,
+        )
+        return mha_out
